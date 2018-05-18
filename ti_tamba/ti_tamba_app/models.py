d1697284667a76aea095c6a4b6f4378410a6b295
Joshua Chipile <chipilej@gmail.com>
2018-05-18 16:23:15 +0200

    Created template for the video website

--- ti_tamba/ti_tamba_app/models.py
+++ ti_tamba/ti_tamba_app/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class VideoLibrary(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    description = models.CharField(max_length=550, blank=True)
+    file = models.FileField(upload_to='videos/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
--- ti_tamba/ti_tamba_app/views.py
+++ ti_tamba/ti_tamba_app/views.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from models import VideoLibrary
+
 # Create your views here.
+
+#Video Library page
+def video_library(request):
+    videos = VideoLibrary.objects.all()
+    return render(request, 'shows/video.html', {'videos' : videos})
